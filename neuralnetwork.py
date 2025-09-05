968099e797ec48a1778a68131e803daf6f8e2f40
AquaBob-15 <90625650+llivshits@users.noreply.github.com>
2025-09-05 18:33:33 -0500

    Finished forward propogation by adding softmax activation function to output neurons. Streamlined design and put biases in neurons to make design more in line with theory. Also various code fixes and added __repr__ for each neuron type

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,48 +1,77 @@
 from typing import List
 import numpy as np
+import math
 
 class InputNeuron():
-    def __init__(self, input):
-        self.input = input
-        self.output = [] 
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.outputs = [] 
         
     def forward_prop(self):
-        for item in output:
-            item.inVal = (input * item.weight) + item.bias
+        for item in self.outputs:
+            item.val = self.inputs
+            
+    def __repr__(self):
+        to_return = "edges: "
+        for item in self.inputs:
+            to_return += item.val + " " + item.weight
+        return to_return
         
 class OutputNeuron():
-    def __init__(self, output):
-        self.input = []     
-        self.output = output
+    def __init__(self, bias):
+        self.inputs = []     
+        self.outputs = []
+        self.bias = bias
         
     def forward_prop(self):
-        sum = 0.0
-        for item in input:
-            sum += (item.outVal * item.weight)
-        output = sum;        
+        total =  0
+        for item in self.inputs:
+            total += math.exp(item.val * item.weight + self.bias)
+        return self.softmax();               
         
-    #def softmax(self):
-            
+    def softmax(self, total):
+        new_outputs = []
     
-class HiddenNeuron()  :
-    def __init__(self):
-        self.input = []     
-        self.output = []
+        for item in self.inputs:
+            new_outputs.append(math.exp((item.val * item.weight + self.bias) / total))
+
+        return new_outputs
+    
+    def __repr__(self):
+        to_return = "edges: "
+        for item in self.inputs:
+            to_return += item.val + " " + item.weight
+        to_return += ". output: "
+        for item in self.outputs: 
+            to_return += item.val
+        return to_return
+    
+class HiddenNeuron():
+    def __init__(self, bias):
+        self.inputs = []     
+        self.outputs = []
+        self.bias = bias
         
     def forward_prop(self):
-        sum = 0.0
-        for item in input:
-            sum += (item.outVal * item.weight)
-        for item in output: 
-           value = (input * item.weight) + item.bias
-           item.inVal = relu(value)
+        total = 0.0
+        for item in self.inputs:
+            total += (item.val * item.weight) 
+        for item in self.outputs: 
+           item.val = self.relu(total + self.bias)
            
     def relu(self, value):
-        return max(0, max)
+        return max(0, value)
+    
+    def __repr__(self):
+        to_return = "input edges: "
+        for item in self.inputs:
+            to_return += item.val + " " + item.weight
+        to_return += ". output edges: "
+        for item in self.outputs: 
+            to_return += item.val + " " + item.weight
+        return to_return
     
 class Edge:
-    def __init__(self, inVal, outVal, bias = 0.0, weight = 0.0):
-        self.bias = bias
+    def __init__(self, val, weight = 0.0):
         self.weight = weight
-        self.inVal = inVal
-        self.outVal = outVal
+        self.val = val
